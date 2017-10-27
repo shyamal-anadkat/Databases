3e07068bf6d86315ce9f66703f24c406536a929a
Shyamal Anadkat <shyamalanadkat@gmail.com>
2017-10-27 14:29:13 -0500

    Project 3 init

---
+++ Project3/Btree/src/btree.cpp
@@ -0,0 +1,86 @@
+/**
+ * @author See Contributors.txt for code contributors and overview of BadgerDB.
+ *
+ * @section LICENSE
+ * Copyright (c) 2012 Database Group, Computer Sciences Department, University of Wisconsin-Madison.
+ */
+
+#include "btree.h"
+#include "filescan.h"
+#include "exceptions/bad_index_info_exception.h"
+#include "exceptions/bad_opcodes_exception.h"
+#include "exceptions/bad_scanrange_exception.h"
+#include "exceptions/no_such_key_found_exception.h"
+#include "exceptions/scan_not_initialized_exception.h"
+#include "exceptions/index_scan_completed_exception.h"
+#include "exceptions/file_not_found_exception.h"
+#include "exceptions/end_of_file_exception.h"
+
+
+//#define DEBUG
+
+namespace badgerdb
+{
+
+// -----------------------------------------------------------------------------
+// BTreeIndex::BTreeIndex -- Constructor
+// -----------------------------------------------------------------------------
+
+BTreeIndex::BTreeIndex(const std::string & relationName,
+		std::string & outIndexName,
+		BufMgr *bufMgrIn,
+		const int attrByteOffset,
+		const Datatype attrType)
+{
+
+}
+
+
+// -----------------------------------------------------------------------------
+// BTreeIndex::~BTreeIndex -- destructor
+// -----------------------------------------------------------------------------
+
+BTreeIndex::~BTreeIndex()
+{
+}
+
+// -----------------------------------------------------------------------------
+// BTreeIndex::insertEntry
+// -----------------------------------------------------------------------------
+
+const void BTreeIndex::insertEntry(const void *key, const RecordId rid) 
+{
+
+}
+
+// -----------------------------------------------------------------------------
+// BTreeIndex::startScan
+// -----------------------------------------------------------------------------
+
+const void BTreeIndex::startScan(const void* lowValParm,
+				   const Operator lowOpParm,
+				   const void* highValParm,
+				   const Operator highOpParm)
+{
+
+}
+
+// -----------------------------------------------------------------------------
+// BTreeIndex::scanNext
+// -----------------------------------------------------------------------------
+
+const void BTreeIndex::scanNext(RecordId& outRid) 
+{
+
+}
+
+// -----------------------------------------------------------------------------
+// BTreeIndex::endScan
+// -----------------------------------------------------------------------------
+//
+const void BTreeIndex::endScan() 
+{
+
+}
+
+}
