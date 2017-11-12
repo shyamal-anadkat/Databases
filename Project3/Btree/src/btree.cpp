ac919ed23f5e948124a66f5eb42f1d2141ee1cd2
zostale <shyamalanadkat@gmail.com>
2017-11-11 19:12:35 -0600

    implement Btree constructor and destructor

--- Project3/Btree/src/btree.cpp
+++ Project3/Btree/src/btree.cpp
@@ -33,6 +33,103 @@ BTreeIndex::BTreeIndex(const std::string & relationName,
 		const Datatype attrType)
 {
 
+	// init fields for Btree
+	this->bufMgr = bufMgrIn; //bufManager instance
+	this->leafOccupancy = INTARRAYLEAFSIZE;
+	this->nodeOccupancy = INTARRAYNONLEAFSIZE;
+
+	this->attributeType = attrType;
+	this->attrByteOffset = attrByteOffset;
+	this->scanExecuting = false;
+
+	// constructing index file name 
+	std::ostringstream idxStr;
+	idxStr << relationName << '.' << attrByteOffset;
+	std::string indexName = idxStr.str(); //indexName : name of index file created
+
+	// return the name of index file after determining it above
+	outIndexName = indexName;
+
+	// debug checks
+	std::cout << "Index File Name: " << indexName << std::endl;
+
+
+	// if index file exists, open the file
+	 
+	if(File::exists(outIndexName)) {
+
+		// open file if exists 
+		this->file = new BlobFile(outIndexName, false);
+		
+		// get meta page info (always the first page)
+		Page *metaPage;
+		this->headerPageNum = file->getFirstPageNo();
+		this->bufMgr->readPage(this->file, headerPageNum, metaPage);
+
+		// cast meta page to IndexMetaInfo structure
+		IndexMetaInfo * indexMetaInfo = (IndexMetaInfo *)metaPage;
+		
+		// unpin the header page, as we don't use it after constructor 
+		this->bufMgr->unPinPage(this->file, headerPageNum, false);
+
+		//// VALIDATION CHECK ////
+        // @throws  BadIndexInfoException     
+		// If the index file already exists for the corresponding attribute,
+		// but values in metapage(relationName, attribute byte offset, attribute type etc.) 
+		// do not match with values received through constructor parameters.
+		if( (indexMetaInfo->attrType != attrType) || 
+			(indexMetaInfo->attrByteOffset != attrByteOffset) ||
+			(indexMetaInfo->relationName != relationName) ) 
+		{
+
+			throw BadIndexInfoException("Error: Index meta attributes don't match!");
+		}
+		// page number of root page of B+ tree inside index file.
+		this->rootPageNum = indexMetaInfo->rootPageNo;
+	}
+
+	else {
+
+		std::cout << "Creating index file ...\n";
+
+		// create index file if doesn't exist + meta pg + root pg
+		this->file = new BlobFile(outIndexName, true);
+		Page* indexMetaInfoPage;
+
+		//// meta info init ////
+		this->bufMgr->allocPage(this->file, headerPageNum, indexMetaInfoPage);
+		struct IndexMetaInfo * metaInfo = (struct IndexMetaInfo *)indexMetaInfoPage;
+		metaInfo->attrByteOffset = attrByteOffset;
+		metaInfo->attrType = attrType;
+		strncpy(metaInfo->relationName, relationName.c_str(), sizeof(metaInfo->relationName));
+
+		//// root page init ////
+		Page* rootPage;
+		this->bufMgr->allocPage(this->file, rootPageNum, rootPage);
+		NonLeafNodeInt* root = (NonLeafNodeInt*) rootPage;
+		metaInfo->rootPageNo = this->rootPageNum = 2; // Root page starts as page 2
+		root->level = 1;
+
+		// unpin headerPage and rootPage
+		bufMgr->unPinPage(file, headerPageNum, true);
+		bufMgr->unPinPage(file, rootPageNum,   true);
+
+		//insert entries for every tuple in the base relation using FileScan class.
+		RecordId curr_rid;
+		FileScan * fs = new FileScan(relationName, bufMgr);
+		try {
+			while(true) {
+				//// scan entry, insert entries here ////
+				fs->scanNext(curr_rid);
+				std::string recordStr = fs->getRecord();
+				const char* recordToInsert = recordStr.c_str();
+				insertEntry(recordToInsert + attrByteOffset, curr_rid);
+			}
+		} catch(EndOfFileException e) { delete fs;}
+
+		bufMgr->flushFile(file);
+		//delete fs;
+	}
 }
 
 
@@ -44,8 +141,10 @@ BTreeIndex::~BTreeIndex()
 {
 	// flushing the index file
 	bufMgr->flushFile(file);
-	// descructor of file class called
+	// destructor of file class called
 	file->~File();
+	delete file;
+	delete bufMgr;
 }
 
 // -----------------------------------------------------------------------------
