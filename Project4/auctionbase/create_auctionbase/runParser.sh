316b9443eb4542668f2581969779394a12312eeb
Avichal Rakesh <avichal.rakesh@gmail.com>
2017-12-01 11:23:56 -0600

    fix paths in scripts

--- Project4/auctionbase/create_auctionbase/runParser.sh
+++ Project4/auctionbase/create_auctionbase/runParser.sh
@@ -1,4 +1,4 @@
-python skeleton_parser.py /usr/class/cs145/project/ebay_data/items-*.json
+python skeleton_parser.py ../ebay_data/items-*.json
 
 sort -u items.dat -o itemsUnique.dat
 sort -u categories.dat -o categoriesUnique.dat
