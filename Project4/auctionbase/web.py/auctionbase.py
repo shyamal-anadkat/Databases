361f2da6be1fffff3e0bcab2fdc878601113b88a
Shyamal H Anadkat <anadkat@wisc.edu>
2017-12-06 01:14:03 -0600

    Add Styling to Search and Seperate Auction View

--- Project4/auctionbase/web.py/auctionbase.py
+++ Project4/auctionbase/web.py/auctionbase.py
@@ -1,12 +1,15 @@
 #!/usr/bin/env python
 
-import sys; sys.path.insert(0, 'lib') # this line is necessary for the rest
-import os                             # of the imports to work!
+import sys;
+
+sys.path.insert(0, 'lib')  # this line is necessary for the rest
+import os  # of the imports to work!
 
 import web
 import sqlitedb
 from jinja2 import Environment, FileSystemLoader
 from datetime import datetime
+
 
 ###########################################################################################
 ##########################DO NOT CHANGE ANYTHING ABOVE THIS LINE!##########################
@@ -24,6 +27,7 @@ from datetime import datetime
 def string_to_time(date_str):
     return datetime.strptime(date_str, '%Y-%m-%d %H:%M:%S')
 
+
 # helper method to render a template in the templates/ directory
 #
 # `template_name': name of template file to render
@@ -39,14 +43,15 @@ def render_template(template_name, **context):
     globals = context.pop('globals', {})
 
     jinja_env = Environment(autoescape=True,
-            loader=FileSystemLoader(os.path.join(os.path.dirname(__file__), 'templates')),
-            extensions=extensions,
-            )
+                            loader=FileSystemLoader(os.path.join(os.path.dirname(__file__), 'templates')),
+                            extensions=extensions,
+                            )
     jinja_env.globals.update(globals)
 
-    web.header('Content-Type','text/html; charset=utf-8', unique=True)
+    web.header('Content-Type', 'text/html; charset=utf-8', unique=True)
 
     return jinja_env.get_template(template_name).render(context)
+
 
 #####################END HELPER METHODS#####################
 
@@ -54,10 +59,12 @@ urls = ('/currtime', 'curr_time',
         '/selecttime', 'select_time',
         '/search', 'search',
         '/add_bid', 'add_bid',
+        '/auction/(.*)', 'auction_detail',
         '/', 'home'
         # TODO: add additional URLs here
         # first parameter => URL, second parameter => class name
         )
+
 
 class curr_time:
     # A simple GET request, to '/currtime'
@@ -66,12 +73,14 @@ class curr_time:
     # in order to have its value displayed on the web page
     def GET(self):
         current_time = sqlitedb.getTime()
-        return render_template('curr_time.html', time = current_time)
+        return render_template('curr_time.html', time=current_time)
+
 
 # temp-home landing page
 class home:
     def GET(self):
         return render_template('home.html')
+
 
 class search:
     def GET(self):
@@ -81,13 +90,13 @@ class search:
         try:
             search_params = web.input()
 
-            item_id   = search_params['itemID']
-            user_id   = search_params['userID']
+            item_id = search_params['itemID']
+            user_id = search_params['userID']
             min_price = search_params['minPrice']
             max_price = search_params['maxPrice']
-            status    = search_params['status']
+            status = search_params['status']
             items = []
-            
+
             items = sqlitedb.getItemsOnSearch(item_id, user_id, min_price, max_price, status)
             # @TODO queries in sqlitedb
             message = 'Success ! Retreived ' + str(len(items)) + ' results.'
@@ -95,7 +104,8 @@ class search:
         except Exception as e:
             message = str(e)
 
-        return render_template('search.html', search_result= items, message= message)
+        return render_template('search.html', search_result=items, message=message)
+
 
 class add_bid:
     def GET(self):
@@ -106,51 +116,52 @@ class add_bid:
             post_params = web.input()
             add_result = ''
             item_id = post_params['itemID']
-            price   = post_params['price']
+            price = post_params['price']
             user_id = post_params['userID']
             current_time = sqlitedb.getTime()
 
             # Validation checks
 
             # make all input fields required
-            if(item_id == '' or price == '' or user_id == ''):
-                return render_template('add_bid.html', message = 'Error: All fields are required')
+            if (item_id == '' or price == '' or user_id == ''):
+                return render_template('add_bid.html', message='Error: All fields are required')
 
-            # don't accept bids on items that don't exist 
-            if(sqlitedb.getItemById(item_id) == None):
-                return render_template('add_bid.html', message = 'Error: Invalid Item ID !')
+            # don't accept bids on items that don't exist
+            if (sqlitedb.getItemById(item_id) == None):
+                return render_template('add_bid.html', message='Error: Invalid Item ID !')
 
             # Don't accept bids from users that don't exist
-            if(sqlitedb.getUserById(user_id) == None):
-                return render_template('add_bid.html', message = 'Error: Invalid User ID !')
+            if (sqlitedb.getUserById(user_id) == None):
+                return render_template('add_bid.html', message='Error: Invalid User ID !')
 
-            # @TODO: add more validation checks 
+            # @TODO: add more validation checks
 
 
-            # insert transaction 
+            # insert transaction
             message = ''
             t = sqlitedb.transaction()
             try:
-                sqlitedb.db.insert('Bids', ItemID = item_id, UserID = user_id, Amount = price, Time = current_time)
-            
+                sqlitedb.db.insert('Bids', ItemID=item_id, UserID=user_id, Amount=price, Time=current_time)
+
             except Exception as e:
                 t.rollback()
                 message = 'Error ! Bid did not get added.'
                 print str(e)
-            
+
             else:
                 t.commit()
                 message = 'Added Bid !'
                 print 'commited ' + str(t)
                 # message = 'insert success'
 
-            # @TODO validations
-            # add_result = 'executed.'
-            
+                # @TODO validations
+                # add_result = 'executed.'
+
         except Exception as e:
             message = str(e)
 
         return render_template('add_bid.html', message=message, add_result=add_result)
+
 
 class select_time:
     # Aanother GET request, this time to the URL '/selecttime'
@@ -175,21 +186,29 @@ class select_time:
         selected_time = '%s-%s-%s %s:%s:%s' % (yyyy, MM, dd, HH, mm, ss)
         update_message = '(Hello, %s. Previously selected time was: %s.)' % (enter_name, selected_time)
         # TODO: save the selected time as the current time in the database
-        # insert transaction 
+        # insert transaction
+
         t = sqlitedb.transaction()
         try:
             _query = 'UPDATE CurrentTime SET Time = $time'
             sqlitedb.db.query(_query, {'time': selected_time})
         except Exception as e:
-            print str(e)
             t.rollback()
+            print str(e)
             update_message = str(e)
         else:
             t.commit()
 
         # Here, we assign `update_message' to `message', which means
         # we'll refer to it in our template as `message'
-        return render_template('select_time.html', message = update_message)
+        return render_template('select_time.html', message=update_message)
+
+class auction_detail:
+    def GET(self, item):
+        auction = web.input(item=None)
+        categories = sqlitedb.getCategoriesByItemId(auction.item)
+        return render_template('auction_detail.html', status = '', bids ='', categories=categories, details='')
+
 
 ###########################################################################################
 ##########################DO NOT CHANGE ANYTHING BELOW THIS LINE!##########################
--- Project4/auctionbase/web.py/sqlitedb.py
+++ Project4/auctionbase/web.py/sqlitedb.py
@@ -1,8 +1,9 @@
 import web
 
 db = web.database(dbn='sqlite',
-        db='AuctionBase' #add your SQLite database filename
-    )
+                  db='AuctionBase'  # add your SQLite database filename
+                  )
+
 
 ######################BEGIN HELPER METHODS######################
 
@@ -11,9 +12,12 @@ db = web.database(dbn='sqlite',
 def enforceForeignKey():
     db.query('PRAGMA foreign_keys = ON')
 
+
 # initiates a transaction on the database
 def transaction():
     return db.transaction()
+
+
 # Sample usage (in auctionbase.py):
 #
 # t = sqlitedb.transaction()
@@ -36,8 +40,9 @@ def getTime():
     results = query(query_string)
     # alternatively: return results[0]['currenttime']
     # print str(results)
-    return results[0].Time # update this as well to match the
-                                  # column name
+    return results[0].Time  # update this as well to match the
+    # column name
+
 
 # returns a single item specified by the Item's ID in the database
 # Note: if the `result' list is empty (i.e. there are no items for a
@@ -50,54 +55,73 @@ def getItemById(item_id):
         return result[0]
     except IndexError:
         return None
-    
+
 
 def getUserById(user_id):
     # rewrite this method to catch the Exception in case `result' is empty
     query_string = 'select * from Users where UserID = $userID'
-    try: 
+    try:
         result = query(query_string, {'userID': user_id})
         return result[0]
     except IndexError:
         return None
 
+
+def getStatusByItemId(item_id):
+    curr_time = getTime()
+    query_string = 'select '
+    try:
+        result = query(query_string, {})
+    # return result[0]
+    except IndexError:
+        return None
+
+def getCategoriesByItemId(item_id):
+    query_string = 'select Category from Categories where ItemID = $itemID'
+    try:
+        results = query(query_string, {'itemID': item_id})
+        return results
+    except IndexError:
+        return None
+
+
 # wrapper method around web.py's db.query method
 # check out http://webpy.org/cookbook/query for more info
-def query(query_string, vars = {}):
+def query(query_string, vars={}):
     return list(db.query(query_string, vars))
+
 
 #####################END HELPER METHODS#####################
 
-#TODO: additional methods to interact with your database,
+# TODO: additional methods to interact with your database,
 # e.g. to update the current time
 
 def getItemsOnSearch(itemID='', userID='', minPrice='', maxPrice='', status=''):
-
     _query = 'SELECT * FROM Items'
     no_params = (itemID == '' and userID == '' and minPrice == '' and maxPrice == '' and minPrice == '')
 
     if not no_params:
         _query += ' WHERE '
-        if(itemID != ''):
+        if (itemID != ''):
             _query += 'ItemID = ' + itemID
 
-        if(userID != ''):
+        if (userID != ''):
             if (itemID != ''):
                 _query += ' AND '
-            _query += ' Seller_UserID = ' + "'"+userID+"'"
+            _query += ' Seller_UserID = ' + "'" + userID + "'"
 
-        if(minPrice != ''):
-            if (itemID != '' or userID != ''): 
+        if (minPrice != ''):
+            if (itemID != '' or userID != ''):
                 _query += ' AND '
             _query += ' Currently >= ' + minPrice
 
-        if(maxPrice != ''):
-            if (itemID != '' or userID != '' or minPrice != ''): 
+        if (maxPrice != ''):
+            if (itemID != '' or userID != '' or minPrice != ''):
                 _query += ' AND '
             _query += ' Currently <= ' + maxPrice
-                
-        if(status != 'all'):
-            if(itemID != '' or userID != '' or minPrice != '' or maxPrice != ''):
+
+        if (status != 'all'):
+            if (itemID != '' or userID != '' or minPrice != '' or maxPrice != ''):
                 _query += ' AND '
             if status == 'open':
                 _query += '(select Time from CurrentTime) between Started and Ends'
@@ -106,6 +130,6 @@ def getItemsOnSearch(itemID='', userID='', minPrice='', maxPrice='', status=''):
             elif status == 'close':
                 _query += 'Ends < (select Time from CurrentTime)'
 
-    result = query (_query)
-    print _query #debug
+    result = query(_query)
+    print _query  # debug
     return result
